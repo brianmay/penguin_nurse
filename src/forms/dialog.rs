12c4ea94c73d7b48d0a12ec6cde0a1ac7490658c
Brian May <brian@linuxpenguins.xyz>
2025-01-01 09:36:32 +1100

    fix: improve dialog styling on large screens

--- src/forms/dialog.rs
+++ src/forms/dialog.rs
@@ -4,7 +4,7 @@ use dioxus::prelude::*;
 pub fn Dialog(children: Element) -> Element {
     rsx! {
         dialog { class: "modal modal-open w-screen h-screen",
-            div { class: "modal-box w-full h-full max-h-none md:w-100 md:h-100 md:max-h-100",
+            div { class: "modal-box w-full h-full max-h-none md:w-auto md:h-auto md:max-h-[calc(100vh-5em)]",
                 {children}
             }
         }
