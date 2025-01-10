ca4804445a2eeb55cae8def36b0981beda739b79
Brian May <brian@linuxpenguins.xyz>
2025-01-11 09:49:40 +1100

    fix: fix home page today button also

--- src/views/home.rs
+++ src/views/home.rs
@@ -1,19 +1,26 @@
-use chrono::Local;
+use chrono::Utc;
 use dioxus::prelude::*;
 
-use crate::Route;
+use crate::{dt::get_date_for_dt, Route};
 
 #[component]
 pub fn Home() -> Element {
-    let date = Local::now().date_naive();
+    let navigator = navigator();
     rsx! {
         div {
             h1 { "Welcome to Penguin Nurse" }
-            p { "This is a simple web application written in Rust using the Dioxus framework." }
+            p { "This is a  web application written in Rust using the Dioxus framework." }
+            p { "Do not eat." }
             button {
                 class: "btn btn-primary",
                 onclick: move |_| {
-                    navigator().push(Route::TimelineList { date });
+                    let new_date = get_date_for_dt(Utc::now());
+                    if let Ok(new_date) = new_date {
+                        navigator
+                            .push(Route::TimelineList {
+                                date: new_date,
+                            });
+                    }
                 },
                 "Today"
             }
