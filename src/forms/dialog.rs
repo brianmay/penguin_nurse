2c8161572e86acc9d821eb19252525e51940bf08
Brian May <brian@linuxpenguins.xyz>
2025-08-22 08:45:48 +1000

    fix: attempt to get dialog to fit on mobile

--- src/forms/dialog.rs
+++ src/forms/dialog.rs
@@ -3,8 +3,8 @@ use dioxus::prelude::*;
 #[component]
 pub fn Dialog(children: Element) -> Element {
     rsx! {
-        dialog { class: "modal modal-open w-screen h-screen",
-            div { class: "modal-box w-full h-full max-h-none md:w-auto md:h-auto md:max-h-[calc(100vh-5em)]",
+        dialog { class: "modal modal-open w-screen h-[100dvh]",
+            div { class: "modal-box w-full h-full max-h-none md:w-auto md:h-auto md:max-h-[calc(100dvh-5em)]",
                 {children}
             }
         }
